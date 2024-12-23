20fe10f3d60644f48955bb4e5e96627373c6063b
Kobby6-lang <koseitutu43@gmail.com>
2024-12-23 11:39:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,22 @@ menu.onclick = () =>
         nav.classList.toggle('active');
     }
     document.addEventListener("DOMContentLoaded", function() {
-        var lastScrollTop = 0;
         var nav = document.getElementById("nav");
     
         window.addEventListener("scroll", function() {
             var scrollTop = window.scrollY || document.documentElement.scrollTop;
     
-            if (scrollTop > lastScrollTop && scrollTop > 0) {
-                // Scrolling down and not at the top - hide the navbar
-                nav.style.top = "-900px"; // Adjust the value based on your navbar height
-            } else {
-                // Scrolling up - show the navbar
+            if (scrollTop === 0) {
+                // At the top of the page - show the navbar
                 nav.style.top = "0";
+            } else {
+                // Not at the top - hide the navbar
+                nav.style.top = "-900px"; // Adjust the value based on your navbar height
             }
-    
-            lastScrollTop = scrollTop;
         });
     });
+    
+    
         
 
     
